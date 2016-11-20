35192de12e19ad8c72cf5166c59c3b8518fe3643
AnnKrepchenko <anna.krepchenko@gmail.com>
2016-11-20 16:23:43 +0200

    animation splash + status bar fix

--- app/src/main/java/com/krepchenko/yourshoppinglist/ui/activity/SplashActivity.java
+++ app/src/main/java/com/krepchenko/yourshoppinglist/ui/activity/SplashActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -22,16 +23,27 @@ public class SplashActivity extends AppCompatActivity {
 
     private static int SPLASH_TIME_OUT = 3000;
 
-    private Animation operatingAnim, eye_left_Anim, eye_right_Anim;
+    private Animation operatingAnim, eye_left_Anim;
     private View mouse, eye_left, eye_right;
     private EyelidView eyelid_left, eyelid_right;
-
-    // GraduallyTextView mGraduallyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.catloading_main);
+        mouse = findViewById(R.id.mouse);
+        eye_left = findViewById(R.id.eye_left);
+        eye_right = findViewById(R.id.eye_right);
+        eyelid_left = (EyelidView) findViewById(R.id.eyelid_left);
+        eyelid_left.setColor(Color.parseColor("#d0ced1"));
+        eyelid_left.setFromFull(true);
+        eyelid_right = (EyelidView) findViewById(R.id.eyelid_right);
+        initialize();
+        timer.start();
+
+    }
+
+    private void initialize() {
         operatingAnim = new RotateAnimation(360f, 0f, Animation.RELATIVE_TO_SELF, 0.5f,
                 Animation.RELATIVE_TO_SELF, 0.5f);
         operatingAnim.setRepeatCount(Animation.INFINITE);
@@ -42,42 +54,15 @@ public class SplashActivity extends AppCompatActivity {
         eye_left_Anim.setRepeatCount(Animation.INFINITE);
         eye_left_Anim.setDuration(2000);
 
-        eye_right_Anim = new RotateAnimation(360f, 0f, Animation.RELATIVE_TO_SELF, 0.5f,
-                Animation.RELATIVE_TO_SELF, 0.5f);
-        eye_right_Anim.setRepeatCount(Animation.INFINITE);
-        eye_right_Anim.setDuration(2000);
-
         LinearInterpolator lin = new LinearInterpolator();
         operatingAnim.setInterpolator(lin);
         eye_left_Anim.setInterpolator(lin);
-        eye_right_Anim.setInterpolator(lin);
-
-        mouse = findViewById(R.id.mouse);
-
-        eye_left = findViewById(R.id.eye_left);
-
-        eye_right = findViewById(R.id.eye_right);
-
-        eyelid_left = (EyelidView) findViewById(R.id.eyelid_left);
-
-        eyelid_left.setColor(Color.parseColor("#d0ced1"));
-
-        eyelid_left.setFromFull(true);
-
-        eyelid_right = (EyelidView) findViewById(R.id.eyelid_right);
 
         eyelid_right.setColor(Color.parseColor("#d0ced1"));
 
         eyelid_right.setFromFull(true);
         Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Torhok.ttf");
-        ((TextView)findViewById(R.id.app_tv)).setTypeface(typeface);
-        //mGraduallyTextView = (GraduallyTextView) view.findViewById(R.id.graduallyTextView);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startMain();
-            }
-        }, SPLASH_TIME_OUT);
+        ((TextView) findViewById(R.id.app_tv)).setTypeface(typeface);
         operatingAnim.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
@@ -93,35 +78,12 @@ public class SplashActivity extends AppCompatActivity {
                 eyelid_right.resetAnimator();
             }
         });
-    }
-
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        mouse.setAnimation(operatingAnim);
-        eye_left.setAnimation(eye_left_Anim);
-        eye_right.setAnimation(eye_right_Anim);
-        eyelid_left.startLoading();
-        eyelid_right.startLoading();
-//        mGraduallyTextView.startLoading();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-
-        operatingAnim.reset();
-        eye_left_Anim.reset();
-        eye_right_Anim.reset();
-
-        mouse.clearAnimation();
-        eye_left.clearAnimation();
-        eye_right.clearAnimation();
-
-        eyelid_left.stopLoading();
-        eyelid_right.stopLoading();
-        //    mGraduallyTextView.stopLoading();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        startAnimation();
     }
 
 
@@ -129,5 +91,32 @@ public class SplashActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
+        finish();
     }
+
+    private void startAnimation() {
+        mouse.setAnimation(operatingAnim);
+        eye_left.setAnimation(eye_left_Anim);
+        eye_right.setAnimation(eye_left_Anim);
+        eyelid_left.startLoading();
+        eyelid_right.startLoading();
+    }
+
+    private void stopAnimation() {
+        eyelid_left.stopLoading();
+        eyelid_right.stopLoading();
+    }
+
+    private CountDownTimer timer = new CountDownTimer(SPLASH_TIME_OUT, SPLASH_TIME_OUT) {
+        @Override
+        public void onTick(long l) {
+
+        }
+
+        @Override
+        public void onFinish() {
+            stopAnimation();
+            startMain();
+        }
+    };
 }
